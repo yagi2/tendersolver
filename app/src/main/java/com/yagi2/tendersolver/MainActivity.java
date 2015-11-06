345ec8def2f3bc0c2e18b40233a7ff1a946bb5b6
Itsuki Aoyagi <mail@yagi2.com>
2015-11-07 06:39:22 +0900

    マス目と期待値及びsolve結果のマス目をある程度設置（最終調整はロジック等出来てからでOK）

--- app/src/main/java/com/yagi2/tendersolver/MainActivity.java
+++ app/src/main/java/com/yagi2/tendersolver/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /* TextViewの背景色を変更するコード（Edit Textも置き換え可）
+    TextView textView = new TextView(context);
+    textView.setBackgroundColor(0x66ff0000);
+    setContentView(textView);
+     */
 }
